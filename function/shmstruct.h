fb93d2cde9b492d4bea0ba0207ddb612de306d70
makissy <makissy@localhost>
2011-01-24 16:28:10 +0700

    function in CIA

---
+++ function/shmstruct.h
@@ -0,0 +1,11 @@
+
+struct shm_var{
+    CvMat *input3Ch = nullptr;
+	CvMat *output1Ch = nullptr;
+    CvMat *input_morph = nullptr;
+    CvSeq *first_con = nullptr;
+	CvSeq *cur = nullptr;
+	CvMat *out_single = nullptr;
+	CvMat *output_morph = nullptr;
+    
+}
