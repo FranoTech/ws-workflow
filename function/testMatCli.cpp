0b1fb48fd420ba9c1aaf8921e1f948a281664676
makissy <makissy@localhost>
2011-06-15 12:26:20 +0700

    now, we can use sharedmem w/ mat. next plan is to change all code to use shared memory. fighto!!

--- function/testMatCli.cpp
+++ function/testMatCli.cpp
@@ -25,7 +25,7 @@ int main (int argc, char** argv){
     
     
     /* Create the segment */
-    if ((shmid = shmget(key, matSize, IPC_CREAT | 0666)) < 0) {
+    if ((shmid = shmget(key, 10368000, IPC_CREAT | 0666)) < 0) {
         perror("shmget");
         exit(1);
     }
@@ -38,17 +38,22 @@ int main (int argc, char** argv){
     
     mat32FC1->data.ptr = addr;
     
-    //cvNamedWindow("result", CV_WINDOW_AUTOSIZE);
-    //cvShowImage("result", mat32FC1);
-    //cvWaitKey(10000);
-    //cvDestroyWindow("result");
     
     CvSize mat_Size = cvGetSize(mat32FC1);
     cout<<mat32FC1->height<<endl;
     cout<<mat32FC1->width<<endl;
     cout<<"[1,33] = "<<CV_MAT_ELEM(*mat32FC1, float, 1, 33)<<endl;
     
+    IplImage *tmp8UC1 = cvCreateImage(cvGetSize(mat32FC1), IPL_DEPTH_8U, 1);
+    cvConvert(mat32FC1, tmp8UC1);
+    
+    cvNamedWindow("result", CV_WINDOW_AUTOSIZE);
+    cvShowImage("result", tmp8UC1);
+    cvWaitKey(10000);
+    cvDestroyWindow("result");
+    
     cvReleaseMat(&mat32FC1);
+    cvReleaseImage(&tmp8UC1);
     
     return 0;
 }
