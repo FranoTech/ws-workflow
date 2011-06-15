80fdd87ca4d74ed1c99b3e4924fa94844bd04605
makissy <makissy@localhost>
2011-06-15 12:26:20 +0700

    now, we can use sharedmem w/ mat. next plan is to change all code to use shared memory. fighto!!

--- function/testMatSrv.cpp
+++ function/testMatSrv.cpp
@@ -25,6 +25,10 @@ int main (int argc, char** argv){
         cout<<"can not load image";
         exit(1);
     }
+    cout<<"img->imageSize"<<img->imageSize<<endl;
+    cout<<"img->height"<<img->height<<endl;
+    cout<<"img->width"<<img->width<<endl;
+    
     
 	CvMat *mat32FC1 = cvCreateMat(img->height, img->width, CV_32FC1);
 	if(!mat32FC1){
@@ -36,7 +40,7 @@ int main (int argc, char** argv){
 	//IplImage *tmpImage = cvCreateImageHeader(cvSize(img->width, img->height), IPL_DEPTH_8U, 3);
 
     /* Create the segment */
-    if ((shmid = shmget(key, matSize, IPC_CREAT | 0666)) < 0) {
+    if ((shmid = shmget(key, img->imageSize, IPC_CREAT | 0666)) < 0) {
         perror("shmget");
         exit(1);
     }
@@ -51,9 +55,11 @@ int main (int argc, char** argv){
     cvConvertScale(img, mat32FC1);
     
     CvSize mat_Size = cvGetSize(mat32FC1);
+    cout<<"size of mat"<<matSize<<endl;
     cout<<mat32FC1->height<<endl;
     cout<<mat32FC1->width<<endl;
     cout<<"[1,33] = "<<CV_MAT_ELEM(*mat32FC1, float, 1, 33)<<endl;
+    
     
     cvReleaseImage(&img);
     cvReleaseMat(&mat32FC1);
