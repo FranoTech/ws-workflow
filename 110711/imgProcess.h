a9e70c2cec54c71f027aad80aa41a9d750143647
makissy <makissy@localhost>
2011-07-11 11:50:54 +0700

    ?????????????????????????? ???????????? compile ?? ?????????????? -*-

---
+++ 110711/imgProcess.h
@@ -0,0 +1,18 @@
+//gsoap ns service name: imgProcess﻿  
+//gsoap ns service style: rpc
+//gsoap ns service encoding: encoded
+//gsoap ns service namespace: http://localhost/imgProcess.wsdl
+//gsoap ns service location: http://localhost/cgi-bin/imgProcessServer
+
+//gsoap ns schema namespace: urn:img
+
+class ns__ImageData
+{
+    int sharedKey;
+    int imgHeight;
+    int imgWidth;
+};
+
+int ns__Ipl1ChToMat(char *InputFilename, ns__ImageData &out);
+int ns__BinaryThreshold( ns__ImageData &in, double threshold, double maxValue, ns__ImageData &out);
+
