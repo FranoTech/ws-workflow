cccf4301fc6bfb4b19acffe5de90e9edb0ca2f6b
makissy <makissy@localhost>
2011-08-23 12:23:17 +0700

    ????????????????? ?????????? new ????????

---
+++ function/name_gen/name.cpp
@@ -0,0 +1,60 @@
+#include "soapH.h"
+#include "name.nsmap"
+#include <string>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{ 
+    struct soap soap;
+    soap_init(&soap);
+    if (argc < 2)		// no args: assume this is a CGI application
+    { 
+        soap_serve(&soap);	// serve request
+        soap_destroy(&soap);// cleanup class instances
+        soap_end(&soap);	// cleanup
+    }
+    return 0;
+
+}
+
+int ns__genName(struct soap *soap, char *InputFilename, char *&OutputFilename)
+{ 
+    //OutputFilename = new char[50];
+    //*a = (char*)soap_malloc(soap, 11); 
+    *&OutputFilename = (char*)soap_malloc(soap, 60); 
+    if(InputFilename){
+        cerr<<"1"<<endl;
+        time_t now = time(0);
+        strftime(OutputFilename, sizeof(OutputFilename)*60, "/home/lluu/dir/%Y%m%d_%H%M%S_biThreshold.jpg", localtime(&now));
+        cerr<<OutputFilename<<endl;
+    }else{ 
+        cerr<<"File Name require"<<endl;
+        soap_fault(soap);
+        soap->fault->faultstring = "Name required";
+        return SOAP_FAULT;
+    }
+    return SOAP_OK;
+}
+
+int ns__genName2(struct soap *soap, char *InputFilename, char **OutputFilename)
+{ 
+    //OutputFilename = new char[50];
+    //*a = (char*)soap_malloc(soap, 11); 
+    *OutputFilename = (char*)soap_malloc(soap, 60); 
+    if(InputFilename){
+        cerr<<"1"<<endl;
+        time_t now = time(0);
+        strftime(*OutputFilename, sizeof(OutputFilename)*60, "/home/lluu/dir/%Y%m%d_%H%M%S_biThreshold.jpg", localtime(&now));
+        cerr<<*OutputFilename<<endl;
+    }else{ 
+        cerr<<"File Name require"<<endl;
+        soap_fault(soap);
+        soap->fault->faultstring = "Name required";
+        return SOAP_FAULT;
+    }
+    return SOAP_OK;
+}
---
+++ function/name_gen/name.h
@@ -0,0 +1,12 @@
+//gsoap ns service name: name	
+//gsoap ns service style: rpc
+//gsoap ns service encoding: encoded
+//gsoap ns service namespace: http://localhost/name.wsdl
+//gsoap ns service location: http://localhost/cgi-bin/name
+
+//gsoap ns schema namespace: urn:n
+
+
+int ns__genName(char *InputFilename, char *&OutputFilename);
+int ns__genName2(char *InputFilename, char **OutputFilename);
+
