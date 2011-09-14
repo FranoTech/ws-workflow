609abd91b54a23c4a763d97c81367a596d51b64e
makissy <makissy@localhost>
2011-09-14 16:47:47 +0700

    ???????????? ????????? c ??? c++ ?????? & ??????????

--- 110902/imgProcessServer.cpp
+++ 110902/imgProcessServer.cpp
@@ -231,26 +231,29 @@ int ns__removeSmallCell (struct soap *soap, char *InputMatFilename, char *&Outpu
 	CvSeq *first_con = nullptr;
 	CvSeq *cur = nullptr;
 	
-	out_single = cvCreateMat(tmp8UC1->height, tmp8UC1->width, CV_32FC1);
-	cvSetZero(out_single);
-	
-	cvFindContours(tmp8UC1, storage, &first_con, sizeof(CvContour), CV_RETR_EXTERNAL);
-	cur = first_con;
-	while (cur != nullptr) {
-		double area = fabs(cvContourArea(cur));
-		int npts = cur->total;
-		CvPoint *p = new CvPoint[npts];
-		cvCvtSeqToArray(cur, p);
-		if (area < 1500.0) // remove small area
-			cvFillPoly(input_morph, &p, &npts, 1, cvScalar(0.0)); // remove from input
-		else if (area < 7500.0) {
-			cvFillPoly(out_single, &p, &npts, 1, cvScalar(255.0)); // move to single
-			cvFillPoly(input_morph, &p, &npts, 1, cvScalar(0.0)); // remove from input
-		}else
-			cvFillPoly(input_morph, &p, &npts, 1, cvScalar(255.0)); // fill hole
-		delete[] p;
-		cur = cur->h_next;
-	}
+    out_single = cvCreateMat(input_morph->height, input_morph->width, CV_32FC1);
+    cvSetZero(out_single);
+    tmp8UC1 = cvCreateImage(cvGetSize(input_morph), IPL_DEPTH_8U, 1);
+        
+    // remove small cells and fill holes.
+    cvConvert(input_morph, tmp8UC1);
+    cvFindContours(tmp8UC1, storage, &first_con, sizeof(CvContour), CV_RETR_EXTERNAL);
+    cur = first_con;
+    while (cur != nullptr) {
+        double area = fabs(cvContourArea(cur));
+        int npts = cur->total;
+        CvPoint *p = new CvPoint[npts];
+        cvCvtSeqToArray(cur, p);
+        if (area < 1500.0) // remove small area
+            cvFillPoly(input_morph, &p, &npts, 1, cvScalar(0.0)); // remove from input
+        else if (area < 7500.0) {
+            cvFillPoly(out_single, &p, &npts, 1, cvScalar(255.0)); // move to single
+            cvFillPoly(input_morph, &p, &npts, 1, cvScalar(0.0)); // remove from input
+        }else
+            cvFillPoly(input_morph, &p, &npts, 1, cvScalar(255.0)); // fill hole
+        delete[] p;
+        cur = cur->h_next;
+    }
     
     //vector<vector<Point> > contours;
     //findContours(src, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
