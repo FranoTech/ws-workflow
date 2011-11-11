cf82dc5ccc88e35d2fd68d2d0e10b51b4c23efc9
makissy <makissy@localhost>
2011-11-11 13:18:57 +0700

    ?????????? \o/  ??? pos ??????? ?????? R / B ???????????? >= 1 ??????? 255

--- function/split.cpp
+++ function/split.cpp
@@ -9,6 +9,7 @@ using namespace cv;
 int main (int argc, char** argv){
    
     Mat src  = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    src.convertTo(src, CV_32FC3);
     vector<Mat> splited;
     
     cv::split(src, splited); 
@@ -20,42 +21,33 @@ int main (int argc, char** argv){
     
     //R/B
     Mat RB (splited[0].rows, splited[0].cols, CV_32FC1);
-    //cv::divide(splited[2], splited[1], RB, 1);
+    cv::divide(splited[2], splited[1], RB, 1);
     
     
-    for( int y = 0; y < splited[2].rows; y++ )
-    {   for( int x = 0; x < splited[2].cols; x++ )
-        {
-            //if(splited[0].at<int>(y,x) != 0)
-                //RB.at<int>(y,x) = splited[2].at<int>(y,x)/splited[0].at<int>(y,x);
-            //else
-                //RB.at<int>(y,x) = 12345;
+    //for( int y = 0; y < splited[2].rows; y++ )
+    //{   for( int x = 0; x < splited[2].cols; x++ )
+        //{
+            ////if(splited[0].at<int>(y,x) != 0)
+                ////RB.at<int>(y,x) = splited[2].at<int>(y,x)/splited[0].at<int>(y,x);
+            ////else
+                ////RB.at<int>(y,x) = 12345;
                 
-            cout<<(int)splited[2].at<uchar>(y,x)<<" ";
+            //cout<<(int)splited[2].at<uchar>(y,x)<<" ";
             
-        }
-        cout<<endl;
-    }   
-    cout<<"=============================================================================="<<endl;
-    for( int y = 0; y < splited[2].rows; y++ )
-    {   for( int x = 0; x < splited[2].cols; x++ )
-        {
-            cout<<(int)splited[0].at<uchar>(y,x)<<" ";
-            
-        }
-        cout<<endl;
-    } 
-    
-    //for( int y = 0; y < RB.rows; y++ )
-    //{   for( int x = 0; x < RB.cols; x++ ){ 
-            //if(splited[0].at<uchar>(y,x) != 0 && (splited[2].at<uchar>(y,x)/splited[0].at<uchar>(y,x) == 1))
-                //RB.at<uchar>(y,x) = 0;
-            //else
-                //RB.at<uchar>(y,x) = 255;
-            //cout<<(int)RB.at<uchar>(y,x)<<" ";
         //}
         //cout<<endl;
     //}   
+
+    for( int y = 0; y < RB.rows; y++ )
+    {   for( int x = 0; x < RB.cols; x++ ){ 
+            if(splited[0].at<float>(y,x) != 0 && (splited[2].at<float>(y,x)/splited[0].at<float>(y,x) >= 1))
+                RB.at<float>(y,x) = 255;
+            else
+                RB.at<float>(y,x) = 0;
+            //cout<<(float)RB.at<uchar>(y,x)<<" ";
+        }
+        cout<<endl;
+    }   
         
     namedWindow("RB", CV_WINDOW_AUTOSIZE);
     imshow("RB", RB);
