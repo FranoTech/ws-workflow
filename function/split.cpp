9e40d44919f89cc397d4c0580271b45c7e90b983
makissy <makissy@localhost>
2011-11-14 10:22:59 +0700

    remove unnecessary project

--- function/split.cpp
+++ function/split.cpp
@@ -19,33 +19,22 @@ int main (int argc, char** argv){
     * splited[2] = R
     */
     
-    //R/B
     Mat RB (splited[0].rows, splited[0].cols, CV_32FC1);
+    Mat BR (splited[0].rows, splited[0].cols, CV_32FC1);
     Mat result = Mat::zeros(splited[0].rows, splited[0].cols, CV_32FC1);
+    
+    /* find R:B */
     cv::divide(splited[2], splited[0], RB, 1);
     
-    
-    //for( int y = 0; y < splited[2].rows; y++ )
-    //{   for( int x = 0; x < splited[2].cols; x++ )
-        //{
-            ////if(splited[0].at<int>(y,x) != 0)
-                ////RB.at<int>(y,x) = splited[2].at<int>(y,x)/splited[0].at<int>(y,x);
-            ////else
-                ////RB.at<int>(y,x) = 12345;
-                
-            //cout<<(int)splited[2].at<uchar>(y,x)<<" ";
-            
-        //}
-        //cout<<endl;
-    //}   
-    
-    
+    /* find B:R */
+    cv::divide(splited[0], splited[2], BR, 1);
+
 /* Find POS Cell */
     for( int y = 0; y < RB.rows; y++ )
     {   for( int x = 0; x < RB.cols; x++ ){ 
             //if(splited[0].at<float>(y,x) != 0 && (splited[2].at<float>(y,x)/splited[0].at<float>(y,x) >= 1))
-            if(RB.at<float>(y,x) >= 1)
-                result.at<float>(y,x) = 0;
+            if(RB.at<float>(y,x) >= 1 && BR.at<float>(y,x) < 0.9)
+                result.at<float>(y,x) = 0;   // pos cell is black
             else
                 result.at<float>(y,x) = 255;
             //cout<<(float)RB.at<uchar>(y,x)<<" ";
@@ -61,7 +50,8 @@ int main (int argc, char** argv){
     //cout<<"c = "<<c<<endl<<"r = "<<r<<endl;
     src.release();
     splited.clear();
-    RB.release();
+    RB.release();    
+    BR.release();
     result.release();
     
     return 0;
