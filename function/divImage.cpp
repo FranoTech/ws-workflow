e36dc3239a94dd9699537ff3e375688c1acd3ada
makissy <makissy@localhost>
2011-12-06 10:03:50 +0700

    prevent duplicated columns

--- function/divImage.cpp
+++ function/divImage.cpp
@@ -9,15 +9,21 @@ int main(int argc, char **argv)
 {
     Mat src = imread(argv[1],0);
     
-    vector<Mat> result;
-    //Mat result[4];
+    //vector<Mat> result;
+    Mat result[4];
     
     #pragma omp parallel num_threads(4)
     {
-        int numt = omp_get_num_threads(); // 2
-        int tid = omp_get_thread_num(); // 0,1
-        int start = tid*(src.cols/numt);  //0
-        int end = (tid+1)*(src.cols/numt); //3
+        int numt = omp_get_num_threads();       // 2
+        int tid = omp_get_thread_num();         // 0,1
+        int start;
+        if(tid == 0) 
+        {                                       //prevent duplicated data
+            start = tid*(src.cols/numt);        // let's tid[0] start at col[0]
+        } else {
+            start = (tid*(src.cols/numt))+1;    //others start at col[x+1] 
+        }
+        int end = (tid+1)*(src.cols/numt);      //3
         
         
         cout<<"tid = "<<tid<<endl
@@ -26,18 +32,18 @@ int main(int argc, char **argv)
             <<"num thread = "<<numt<<endl
             <<"src.cols = "<<src.cols<<endl; 
             
-        result[tid] = src.colRange(start, end);
+        //result[tid] = src.colRange(start, end);
     }
     
-    namedWindow("src1", CV_WINDOW_AUTOSIZE);
-    imshow("src1", src);
+    //namedWindow("src1", CV_WINDOW_AUTOSIZE);
+    //imshow("src1", src);
     
-    namedWindow("src2", CV_WINDOW_AUTOSIZE);
-    imshow("src2", result[0]);
+    //namedWindow("src2", CV_WINDOW_AUTOSIZE);
+    //imshow("src2", result[0]);
 
-    namedWindow("src3", CV_WINDOW_AUTOSIZE);
-    imshow("src3", result[1]);
-    waitKey(0);
+    //namedWindow("src3", CV_WINDOW_AUTOSIZE);
+    //imshow("src3", result[1]);
+    //waitKey(0);
  
     return 0;
 }
