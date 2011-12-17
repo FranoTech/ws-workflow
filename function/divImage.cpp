98be277336c8e90cb637c8b11de8aab6f5e0daf4
makissy <makissy@localhost>
2011-12-18 00:13:04 +0700

    ????????????? ??? omp ??? convertTo ??????

--- function/divImage.cpp
+++ function/divImage.cpp
@@ -9,44 +9,46 @@ int main(int argc, char **argv)
 {
     Mat src = imread(argv[1],0);
     
-    //vector<Mat> result;
-    Mat divSrc[4];
-    Mat result[4];
-    
-    #pragma omp parallel shared(src) num_threads(4)
+    //~ Mat divSrc[4];
+    #pragma omp parallel shared(src) num_threads(2)
     {
         int numt = omp_get_num_threads();       // 2
         int tid = omp_get_thread_num();         // 0,1
-        int start;
-        if(tid == 0) 
-        {                                       //prevent duplicated data
-            start = tid*(src.cols/numt);        // let's tid[0] start at col[0]
-        } else {
-            start = (tid*(src.cols/numt))+1;    //others start at col[x+1] 
+        int start = tid*(src.cols/numt); //t0 = 0  t1 = 3
+        int end = (tid+1)*(src.cols/numt);// t0 = 3 t1 = 6
+        if( tid == (numt-1) )
+        {
+            end = src.cols;
         }
-        int end = (tid+1)*(src.cols/numt);      //3
-        
-        
-        //~ cout<<"tid = "<<tid<<endl
-            //~ <<"start = "<<start<<endl
-            //~ <<"end = "<<end<<endl
-            //~ <<"num thread = "<<numt<<endl
-            //~ <<"src.cols = "<<src.cols<<endl; 
+
+        cout<<"tid = "<<tid<<endl
+            <<"start = "<<start<<endl
+            <<"end = "<<end<<endl
+            <<"num thread = "<<numt<<endl
+            <<"src.cols = "<<src.cols<<endl; 
             
-        divSrc[tid] = src.colRange(start, end);
-        threshold(divSrc[tid], result[tid], 127.0, 255.0, THRESH_BINARY);
+        Mat divSrc = src.colRange(start, end);
+        
+        //~ threshold(divSrc[tid], divSrc[tid], 127.0, 255.0, THRESH_BINARY);
+        threshold(divSrc, divSrc, 127.0, 255.0, THRESH_BINARY);
         
     }
+    src.convertTo(src, CV_8UC1);
+    if(!imwrite("divomp.jpg", src))
+    {
+        cout<<"write error"<<endl;
+    }
     
-    namedWindow("src1", CV_WINDOW_AUTOSIZE);
-    imshow("src1", src);
+    //~ namedWindow("src1", CV_WINDOW_AUTOSIZE);
+    //~ imshow("src1", src);
     
-    namedWindow("src2", CV_WINDOW_AUTOSIZE);
-    imshow("src2", result[0]);
-
-    namedWindow("src3", CV_WINDOW_AUTOSIZE);
-    imshow("src3", result[1]);
-    waitKey(0);
+    //~ namedWindow("src2", CV_WINDOW_AUTOSIZE);
+    //~ imshow("src2", result[0]);
+//~ 
+    //~ namedWindow("src3", CV_WINDOW_AUTOSIZE);
+    //~ imshow("src3", result[1]);
+    
+    src.release();
  
     return 0;
 }
