c661bbb4ba2bdaf8b1aaf4c31d197f0f4c3dc69f
makissy <makissy@localhost>
2011-12-20 17:28:33 +0700

    still got wrong result

--- maliProject/maliServer.cpp
+++ maliProject/maliServer.cpp
@@ -741,72 +741,196 @@ int ns__scanningCell(struct soap *soap,
 // @return char **outputMatFilename
 //
 
-int ns__doWaterShed(struct soap *soap,
-						char *inputMatFilename,
+int ns__separateCell(struct soap *soap,
+						char *input1,
+                        char *input2,
 						char **OutputMatFilename)
 {
     double start, end;
     start = omp_get_wtime();
 
-	Mat src;
-    if(!readMat(inputMatFilename, src))
+    Mat src;
+    if(!readMat(input1, src))
     {
-        soap_fault(soap);
-        cerr << "waterShed :: can not read bin file" << endl;
-        soap->fault->faultstring = "waterShed :: can not read bin file";
-        return SOAP_FAULT;
+        cerr << "trainANN :: can not read bin file" << endl;
+        return soap_receiver_fault(soap, "trainANN :: can not read bin file", NULL);
     }
 
+    // convert src to CvMat to use an old-school function
+    CvMat tmp = src;
+    CV_Assert(tmp.cols == src.cols && tmp.rows == src.rows &&
+        tmp.data.ptr == (uchar*)src.data && tmp.step == src.step);
     
-    Mat tmp = Mat(src.rows, src.cols, CV_32FC1);
-    src.convertTo(tmp,CV_32FC1);
+    CvMat *out_single = cvCreateMat(src.rows, src.cols, CV_32FC1);
+    cvConvert(&tmp, out_single);
     
-    if( src.type() != CV_8UC1)
-    {
-        src.convertTo(src, CV_8UC1);
-    }
+    IplImage *tmp8UC1 = cvCreateImage(cvGetSize(out_single), IPL_DEPTH_8U, 1);
+    cvConvert(out_single, tmp8UC1);
     
-	vector<vector<Point> > contours;
-    double area = 0;
-    findContours(	src, contours, CV_RETR_EXTERNAL,
-					CV_CHAIN_APPROX_SIMPLE, Point(0,0));
-
+    CvMemStorage *storage = cvCreateMemStorage();
+	CvSeq *first_con = NULL;
+	CvSeq *cur = NULL;
+    
+    cvFindContours(tmp8UC1, storage, &first_con, sizeof(CvContour), CV_RETR_EXTERNAL);
     int count = 1;
+    cur = first_con;
+    while (cur != NULL) {
+        int npts = cur->total;
+        CvPoint *p = new CvPoint[npts];
+        cvCvtSeqToArray(cur, p);
+        cvFillPoly(out_single, &p, &npts, 1, cvScalar((count++%254)+1)); // fill label, must be 1-255
+        delete[] p;
+        cur = cur->h_next;
+    }
     
-    for(size_t i = 0; i< contours.size(); i++)
+    cerr<<"src2 findcontour"<<endl;
+
+
+    Mat src2;
+    if(!readMat(input2, src2))
     {
-		const Point* p = &contours[i][0];
-        int n = (int)contours[i].size();
-		area = contourArea(Mat(contours[i]));
-
-		if(area < 1500.0) //lower bound
-		{
-			fillPoly( tmp, &p, &n, 1, Scalar((count++%254)+1)); 
-		}
-	}
-
-	contours.clear();
+        cerr << "sep :: can not read bin file" << endl;
+        return soap_receiver_fault(soap, "sep :: can not read bin file", NULL);
+    }
+    CvMat tmp2 = src2;
+    CV_Assert(tmp2.cols == src2.cols && tmp2.rows == src2.rows &&
+        tmp2.data.ptr == (uchar*)src2.data && tmp2.step == src2.step);
     
-    tmp.convertTo(src,CV_8UC1);
+    CvMat *output_morph = cvCreateMat(src2.rows, src2.cols, CV_32FC1);
+    cvConvert(&tmp, output_morph);
+    cvConvertScale(output_morph, tmp8UC1);
     
-    Mat inWater = Mat(tmp.rows, tmp.cols, CV_8UC3);
-    Mat outWater = 
-    
+    CvMat *inwater = cvCreateMat(out_single->height, out_single->width, CV_8UC3);
+    CvMat outwater = cvMat(out_single->height, out_single->width, CV_32SC1, out_single->data.fl);
 
+    cvMerge(tmp8UC1, tmp8UC1, tmp8UC1, NULL, inwater);
+    cvWatershed(inwater, &outwater);
+    
+    cvErode(out_single, out_single, NULL, 2);
+    cvConvertScale(output_morph, tmp8UC1);
+    cvSub(output_morph, out_single, output_morph, tmp8UC1);
+
+    
+    Mat result = cvarrToMat(output_morph, false);
+    
 	/* generate output file name */
     *OutputMatFilename = (char*)soap_malloc(soap, FILENAME_SIZE);
-    getOutputFilename(OutputMatFilename,"_waterShed");
+    getOutputFilename(OutputMatFilename,"_sep");
 
     /* save to bin */
-    if(!saveMat(*OutputMatFilename, output))
+    if(!saveMat(*OutputMatFilename, result))
     {
-        cerr << "waterShed:: save mat to binary file" << endl;
-        return soap_receiver_fault(soap, "waterShed:: save mat to binary file", NULL);
+        cerr << "sep:: save mat to binary file" << endl;
+        return soap_receiver_fault(soap, "sep:: save mat to binary file", NULL);
     }
 
     src.release();
-    tmp.release();
-    outSingle.release();
+    src2.release();
+    cvReleaseMat(&out_single);
+    cvReleaseMat(&output_morph);
+    cvReleaseMat(&inwater);
+    cvReleaseImage(&tmp8UC1);
+
+    end = omp_get_wtime();
+    cerr<<"ns__waterShed time elapsed "<<end-start<<endl;
+    return SOAP_OK;
+}
+
+
+int ns__prepareResult(struct soap *soap,
+						char *inputMatFilename,
+                        char *afterthresNN,
+						char **OutputMatFilename)
+{
+    double start, end;
+    start = omp_get_wtime();
+
+    Mat src;
+    if(!readMat(inputMatFilename, src))
+    {
+        cerr << "prepareResult :: can not read bin file" << endl;
+        return soap_receiver_fault(soap, "prepareResult :: can not read bin file", NULL);
+    }
+
+    // convert src to CvMat to use an old-school function
+    CvMat tmp = src;
+    CV_Assert(tmp.cols == src.cols && tmp.rows == src.rows &&
+        tmp.data.ptr == (uchar*)src.data && tmp.step == src.step);
+    
+    CvMat *output_morph = cvCreateMat(src.rows, src.cols, CV_32FC1);
+    cvConvert(&tmp, output_morph);
+    
+    //***************************//
+    Mat output1ChSrc;
+    if(!readMat(afterthresNN, output1ChSrc))
+    {
+        cerr << "output1Ch:: can not read bin file" << endl;
+        return soap_receiver_fault(soap, "output1Ch :: can not read bin file", NULL);
+    }
+
+    // convert src to CvMat to use an old-school function
+    CvMat tmp2 = output1ChSrc;
+    CV_Assert(tmp2.cols == output1ChSrc.cols && tmp2.rows == output1ChSrc.rows &&
+        tmp2.data.ptr == (uchar*)output1ChSrc.data && tmp2.step == output1ChSrc.step);
+    
+    CvMat *output1Ch = cvCreateMat(tmp2.rows, tmp2.cols, CV_32FC1);
+    cvConvert(&tmp2, output1Ch);
+    
+    //**********************//
+    
+    
+    IplImage *tmp8UC1 = cvCreateImage(cvGetSize(output_morph), IPL_DEPTH_8U, 1);
+    cvConvertScale(output_morph, tmp8UC1);
+    
+    CvMemStorage *storage = cvCreateMemStorage();
+	CvSeq *first_con = NULL;
+	CvSeq *cur = NULL;
+    
+    cvFindContours(tmp8UC1, storage, &first_con, sizeof(CvContour), CV_RETR_EXTERNAL);
+    
+    IplImage *tmpImage = cvCreateImage(cvSize(tmp8UC1->width, tmp8UC1->height), IPL_DEPTH_8U, 3);
+    cvSet(tmpImage, CV_RGB(0,0,255)); // Background, blue
+
+    cvSetZero(tmp8UC1);
+    CvScalar pixel;
+    cur = first_con;
+    //~ int ncell = 0; // total cells
+    
+    while (cur != NULL) {
+        if ((cur->total > 2) && (fabs(cvContourArea(cur)) > 1500.0)) { // remove small area
+            int npts = cur->total;
+            CvPoint *p = new CvPoint[npts];
+            cvCvtSeqToArray(cur, p);
+            cvFillPoly(tmp8UC1, &p, &npts, 1, cvScalar(255)); // set mask
+            pixel = cvAvg(output1Ch, tmp8UC1);
+            cvFillPoly(tmp8UC1, &p, &npts, 1, cvScalar(0)); // clear mask
+            if (pixel.val[0] > 0.5) { // Negative, green
+                if (tmpImage != NULL)
+                    cvFillPoly(tmpImage, &p, &npts, 1, CV_RGB(0,255,0));
+                    
+            } else if (pixel.val[0] > -0.5) { // Positive, red
+                if (tmpImage != NULL)
+                    cvFillPoly(tmpImage, &p, &npts, 1, CV_RGB(255,0,0));
+            }
+            delete[] p;
+        }
+        cur = cur->h_next;
+    }
+    
+    Mat result = cvarrToMat(tmpImage, false);
+
+	/* generate output file name */
+    *OutputMatFilename = (char*)soap_malloc(soap, FILENAME_SIZE);
+    getOutputFilename(OutputMatFilename,"_result");
+
+    /* save to bin */
+    if(!saveMat(*OutputMatFilename, result))
+    {
+        cerr << "result:: save mat to binary file" << endl;
+        return soap_receiver_fault(soap, "result:: save mat to binary file", NULL);
+    }
+
+    src.release();
 
     end = omp_get_wtime();
     cerr<<"ns__waterShed"<<"time elapsed "<<end-start<<endl;
