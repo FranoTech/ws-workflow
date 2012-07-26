f9ab4d137cd580dd0a9c45c2d74d350aac7cabe7
makissy <makissy@localhost>
2012-07-26 10:55:23 +0700

    add makeService script

---
+++ standalone/service/MatToJPG.cpp
@@ -0,0 +1,42 @@
+//
+// name: MatToJPG
+// - convert nat to 8U and save as JPG
+// @param char *InputMatFilename
+// @return char **OutputMatFilename
+
+int ns__MatToJPG (struct soap *soap, char *InputMatFilename, char **OutputMatFilename)
+{
+    double start, end;
+    start = omp_get_wtime();
+
+    Mat src;
+    if(!readMat(InputMatFilename, src))
+    {
+        cerr<< "MatToJPG:: can not read bin file" << endl;
+        return soap_receiver_fault(soap, "MatToJPG:: can not read bin file", NULL);
+    }
+
+    /* check if it is not 8U then convert to 8UC(n) */
+    int chan = src.channels();
+    if( src.type() != 0 || src.type() != 8 || src.type() != 16 )
+    {
+       src.convertTo(src, CV_8UC(chan));
+    }
+
+    /* generate output file name */
+	*OutputMatFilename = (char*)soap_malloc(soap, FILENAME_SIZE);
+    getOutputFilename(OutputMatFilename,".jpg");
+
+    if(!imwrite(*OutputMatFilename, src))
+    {
+        cerr<< "MatToJPG:: can not save mat to jpg file" << endl;
+        return soap_receiver_fault(soap, "MatToJPG:: can not save mat to jpg file", NULL);
+    }
+
+    src.release();
+
+    end = omp_get_wtime();
+    cerr<<"ns__MatToJPG "<<"time elapsed "<<end-start<<endl;
+
+    return SOAP_OK;
+}
---
+++ standalone/service/morphologyEx.cpp
@@ -0,0 +1,58 @@
+//
+// name: ns__MorphologyEx
+// @param
+//      char *InputMatFilename,
+//      char *morphOperation="MORPH_OPEN",
+// @return
+//      char **OutputMatFilename=NULL
+
+int ns__morphologyEx( struct soap *soap,
+                    char *InputMatFilename,
+                    char *morphOperation="MORPH_OPEN",
+                    char **OutputMatFilename=NULL)
+{
+    double start, end;
+    start = omp_get_wtime();
+
+    /* read from bin */
+    Mat src;
+    if(!readMat(InputMatFilename, src))
+    {
+        cerr << "MorphologyEx:: can not read bin file" << endl;
+        return soap_receiver_fault(soap, "MorphologyEx:: can not read bin file", NULL);
+    }
+
+    Mat dst(src.rows, src.cols, src.depth());
+    Mat se;
+    Size seSize(3, 3);
+    Point seAnc(1, 1);
+    int opt = getMorphOperation(morphOperation);
+
+    se = getStructuringElement(MORPH_ELLIPSE, seSize, seAnc);
+    morphologyEx(src, dst, opt, se, seAnc);
+
+    if(src.empty()) {
+			cerr << "MorphologyEx :: something's wrong" << endl;
+            return soap_receiver_fault(soap, "MorphologyEx:: something's wrong", NULL);
+    }
+
+    /* generate output file name */
+    *OutputMatFilename = (char*)soap_malloc(soap, FILENAME_SIZE);
+    getOutputFilename(OutputMatFilename,"_MorphologyEx");
+
+    /* save to bin */
+    if(!saveMat(*OutputMatFilename, dst))
+    {
+        cerr << "MorphologyEx:: can not save mat to binary file" << endl;
+        return soap_receiver_fault(soap, "MorphologyEx:: can not save mat to binary file", NULL);
+    }
+
+    src.release();
+    dst.release();
+    se.release();
+
+    end = omp_get_wtime();
+    cerr<<"ns__MorphologyEx time elapsed "<<end-start<<endl;
+
+    return SOAP_OK;
+}
