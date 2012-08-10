89ec7d95798fd0aebb3113278af30ff81c6c0221
makissy <makissy@localhost>
2012-08-10 10:59:14 +0700

    try to add enable option to myLog.h

--- function/testMylog.cpp
+++ function/testMylog.cpp
@@ -2,11 +2,13 @@
 
 int main ()
 {
+	LogEn;
+	LOG_FILENAME = "test";
 	const int count = 3;
 	Log(logDEBUG) << "A loop with " << count << " iterations" <<std::endl;
 	for (int i = 0; i != count; ++i)
 	{
-		Log(logDEBUG) << "the counter i = " << i;
+		Log(logDEBUG) << "the counter i = " << i << std::endl;
 	}
 	return 0;
 }
